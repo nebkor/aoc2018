cc0a73af493bf4b078b798377267343ce426d833
Joe Ardent <code@ardent.nebcorp.com>
2018-12-01 11:19:57 -0800

    Got the first answer, but the second is not working.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use clap::clap_app;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Result};
+
+fn main() -> Result<()> {
+    let input_file: String = clap_app!(day1 =>
+                                (@arg INPUT: +required "What file has the input.")
+    )
+    .get_matches()
+    .value_of("INPUT")
+    .unwrap()
+    .into();
+
+    let mut set = HashSet::new();
+    let mut sum = 0;
+
+    let file = File::open(input_file)?;
+    for line in BufReader::new(file).lines() {
+        let x: i32 = line.unwrap().parse().unwrap();
+        sum += x;
+        println!("current sum: {}", sum);
+        if !set.insert(sum) {
+            println!("Failed to insert {}", sum);
+        }
+    }
+    println!("{}", sum);
+    Ok(())
 }
