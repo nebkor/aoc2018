8e2aed5cc28b8f9442c820726b95ffe86d0ae6f9
Joe Ardent <code@ardent.nebcorp.com>
2018-12-02 15:29:57 -0800

    Done with day 2.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -21,6 +21,24 @@ fn has_n_chars(s: &str, n: u32) -> bool {
     false
 }
 
+fn hamming(s1: &str, s2: &str) -> usize {
+    let ret: usize = s1
+        .chars()
+        .zip(s2.chars())
+        .map(|(x, y)| if x == y { 0 } else { 1 })
+        .sum();
+
+    ret + (s1.len() as isize - s2.len() as isize).abs() as usize
+}
+
+fn common(s1: &str, s2: &str) -> String {
+    s1.chars()
+        .zip(s2.chars())
+        .filter(|(x, y)| x == y)
+        .map(|(x, _)| x)
+        .collect()
+}
+
 fn main() -> Result<()> {
     let input_file: String = clap_app!(day2 =>
                                 (@arg INPUT: +required "What file has the input.")
@@ -36,10 +54,22 @@ fn main() -> Result<()> {
     let lines2: Vec<_> = lines.iter().filter(|x| has_n_chars(x, 2)).collect();
     let twos = lines2.len();
 
-    let lines3: Vec<_> = lines.iter().filter(|x| has_n_chars(x, 3)).collect();
+    let mut lines3: Vec<_> = lines.iter().filter(|x| has_n_chars(x, 3)).collect();
     let threes = lines3.len();
 
+    lines3.extend(lines2.into_iter());
+
     println!("3s: {}, 2s: {}, 3s * 2s = {}", threes, twos, threes * twos);
+
+    for i in 0..lines3.len() {
+        let base = lines3[i];
+        for s in lines3[i..].iter() {
+            match hamming(base, s) {
+                1 => println!("{}", common(s, base)),
+                _ => (),
+            }
+        }
+    }
 
     Ok(())
 }
