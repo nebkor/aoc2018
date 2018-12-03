e4f6035580d38d8779ce50f0a82cb85cb19a4405
Joe Ardent <code@ardent.nebcorp.com>
2018-12-03 15:48:06 -0800

    Correctly finds the answer to part 2, day 3.
    Keeps track of a lot of extra state and does some wasteful computing. Better
    solution in a few minutes.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,8 +1,10 @@
 use lazy_static::*;
 use regex::Regex;
 use std::collections::{HashMap, HashSet};
+use std::fmt::{self, Display};
 use utils::*;
 
+#[derive(Debug, Hash, PartialOrd, PartialEq, Eq, Clone, Copy)]
 struct Claim {
     pub id: u32,
     xrange: (u32, u32),
@@ -44,25 +46,64 @@ impl Claim {
     }
 }
 
+impl Display for Claim {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "(id: {}, starting at {}, {})",
+            self.id, self.xrange.0, self.yrange.0
+        )
+    }
+}
+
 fn main() {
     let input = get_input("day3");
 
     let file = read_file(&input);
 
-    let mut hmap: HashMap<(u32, u32), HashSet<u32>> = HashMap::new(); // coordinates to IDs
+    let mut cloth_map: HashMap<(u32, u32), HashSet<u32>> = HashMap::new(); // coordinates to IDs
+    let mut claim_map: HashMap<u32, Claim> = HashMap::new();
 
     for entry in file.lines() {
         let claim = Claim::new(&entry.unwrap());
+        claim_map.insert(claim.id, claim.clone());
 
         for x in claim.startx()..claim.endx() {
             for y in claim.starty()..claim.endy() {
-                hmap.entry((x, y))
+                cloth_map
+                    .entry((x, y))
                     .or_insert(HashSet::new())
                     .insert(claim.id);
             }
         }
     }
 
-    let tot: u32 = hmap.values().map(|s| if s.len() > 1 { 1 } else { 0 }).sum();
-    println!("Found {} square inches multiply-claimed.", tot)
+    let mut singles: HashSet<u32> = HashSet::new();
+
+    let tot: u32 = cloth_map
+        .values()
+        .map(|s| match s.len() {
+            x if x > 1 => 1,
+            1 => {
+                for id in s.iter() {
+                    singles.insert(*id);
+                }
+                0
+            }
+            _ => 0,
+        })
+        .sum();
+    println!("Found {} square inches multiply-claimed.", tot);
+
+    'claim: for id in singles.iter() {
+        let claim = claim_map.get(id).unwrap();
+        for x in claim.startx()..claim.endx() {
+            for y in claim.starty()..claim.endy() {
+                if cloth_map.get(&(x, y)).unwrap().len() > 1 {
+                    continue 'claim;
+                }
+            }
+        }
+        println!("Only {} made it through intact.", claim)
+    }
 }
