9e78fd84073d8f9f4f034949791633bfbdcc9033
Joe Ardent <code@ardent.nebcorp.com>
2018-12-04 18:30:14 -0800

    Solves day 4, part 1.

---
+++ day4/src/main.rs
@@ -0,0 +1,79 @@
+use chrono::prelude::*;
+use chrono::Duration;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::collections::HashMap;
+//use utils::*;
+
+//type DateTime = chrono::DateTime<Utc>;
+type Id = u32;
+
+const DSTRING: &str = "%Y-%m-%d %H:%M";
+
+fn main() {
+    lazy_static! {
+        static ref GUARD: Regex = Regex::new(r"\[(.*)\] Guard \#(\d+) begins shift").unwrap();
+        static ref SLEEPS: Regex = Regex::new(r"\[(.*)\] falls asleep").unwrap();
+        static ref WAKES: Regex = Regex::new(r"\[(.*)\] wakes up").unwrap();
+    }
+    let ONE_MINUTE: Duration = Duration::minutes(1);
+
+    let input = include_str!("../input");
+
+    let mut sleeps: HashMap<Id, HashMap<u32, u32>> = HashMap::new();
+
+    let mut id: Id = 0;
+    let mut start_sleep = Utc.ymd(1500, 1, 1).and_hms(0, 0, 0);
+
+    for line in input.lines() {
+        if let Some(start) = GUARD.captures(line) {
+            id = start[2].parse().unwrap();
+        }
+
+        if let Some(sleep) = SLEEPS.captures(line) {
+            start_sleep = Utc.datetime_from_str(&sleep[1], DSTRING).unwrap();
+        }
+
+        if let Some(wake) = WAKES.captures(line) {
+            let end = Utc.datetime_from_str(&wake[1], DSTRING).unwrap();
+            let mut t = start_sleep.clone();
+            while t != end {
+                let minute: u32 = t.format("%M").to_string().parse().unwrap();
+                let mut v = sleeps.entry(id).or_insert(HashMap::new());
+                *(v.entry(minute).or_insert(0)) += 1;
+                t = t.checked_add_signed(ONE_MINUTE).unwrap();
+            }
+        }
+    }
+
+    let mut max_sleep = 0;
+    let mut big_sleeper = 0;
+    for (k, v) in sleeps.iter() {
+        let mut s = 0;
+        for c in v.values() {
+            s += c;
+        }
+        if s > max_sleep {
+            max_sleep = s;
+            big_sleeper = *k;
+        }
+    }
+
+    let mut sleepiest_minute = 0;
+    let mut count = 0;
+
+    for (k, v) in sleeps[&big_sleeper].iter() {
+        if *v > count {
+            count = *v;
+            sleepiest_minute = *k;
+        }
+    }
+
+    // part 1 answer:
+    println!(
+        "Guard {} sleeps most at {}, for a checksum of {}",
+        big_sleeper,
+        sleepiest_minute,
+        big_sleeper * sleepiest_minute
+    );
+}
