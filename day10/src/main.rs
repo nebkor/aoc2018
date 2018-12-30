87855243c5c8747d43708364381d2bbbdaa617f5
Joe Ardent <code@ardent.nebcorp.com>
2018-12-29 16:41:04 -0800

    Just a minor fiddle with day10.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -6,11 +6,13 @@ fn main() {
     let sky = Sky { beacons: beacons };
 
     let mut ymin = MAX;
-    for t in 0..MAX {
+    for t in 0.. {
         let (_, r) = &sky.t(t);
         let ny = r.yspan;
         if ny > ymin {
-            println!("{}:\n{}", t - 1, sky.print(t - 1));
+            for nt in (t - 5)..(t + 5) {
+                println!("{}:\n{}", nt, sky.print(nt));
+            }
             break;
         }
         ymin = ny;
