ff571963e5df0be57e45d1d281e8ef48cf2d5267
Joe Ardent <code@ardent.nebcorp.com>
2019-01-20 21:46:37 -0800

    Differently wrong and complicated solution for day 12.
    This is bad code. It's hard to understand, it's hard to extend, it's hard to modify.
    Plus it's wrong. Relying on the builtin windows() iterator was a mistake.

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,103 +1,74 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
-type Rules = HashMap<Vec<bool>, bool>;
-type Pots = VecDeque<bool>;
+type Rules = HashMap<String, char>;
 
-fn beginning(cur: &Pots, idx: usize, rules: &Rules, new: &mut Pots) -> isize {
-    // only act on first call, and handle up to 2
+fn beginning(cur: &str, new: &mut String, rules: &Rules, leftest: &mut isize, idx: usize) {
     if idx != 0 {
-        return 0;
+        return;
     }
+    let mut nleft: isize = 0;
+    for neg in 1..=2 {
+        let r = match neg {
+            1 => ["...", &cur[0..2]].concat().to_owned(),
+            2 => ["....", &cur[0..1]].concat().to_owned(),
+            _ => unreachable!(),
+        };
 
-    let mut negs: isize = 0;
-
-    for neg in 1..=3 {
-        let p: Vec<bool> = vec![false; 4 - neg]
-            .iter()
-            .chain(cur.iter().take(3 - neg))
-            .map(|b| *b)
-            .collect();
-        if let Some(&pot) = rules.get(&p) {
-            if pot {
-                new.push_front(true);
-                negs += 1;
+        if let Some(&p) = rules.get(&r) {
+            new.insert(0, p);
+            if p == '#' {
+                nleft = neg as isize;
             }
         }
     }
 
-    for pos in 0..=1 {
-        let p: Vec<bool> = vec![false; 2 - pos]
-            .iter()
-            .chain(cur.iter().skip(pos).take(3 - pos))
-            .map(|b| *b)
-            .collect();
-        if let Some(&pot) = rules.get(&p) {
-            new.push_back(pot);
-        } else {
-            new.push_back(cur[pos]);
+    let mut first_plant = -1;
+
+    for pot in 0..2 {
+        let r = match pot {
+            0 => ["..", &cur[0..3]].concat().to_owned(),
+            1 => [".", &cur[0..4]].concat().to_owned(),
+            _ => unreachable!(),
+        };
+
+        if let Some(&p) = rules.get(&r) {
+            new.push(p);
+            if p == '#' {
+                first_plant = pot;
+            }
+            if nleft == 0 && p == '.' && first_plant < 0 {
+                *leftest += 1;
+            }
         }
     }
 
-    negs
+    *leftest -= nleft;
+
+    dbg!(&new);
+
+    //dbg!(leftest);
 }
 
-fn ending(cur: &Pots, start_from: usize, rules: &Rules, new: &mut Pots) {
-    let mut last_pot_checked = start_from + 3;
-    let clen = cur.len();
-    while last_pot_checked < clen {
-        let leftc = cur
-            .iter()
-            .skip(last_pot_checked - 2)
-            .take(3)
-            .map(|b| *b)
-            .collect::<Vec<bool>>();
-        let rightmost = last_pot_checked + 2;
-        if rightmost >= clen {
-            let fill = (rightmost - clen) + 1;
-            let pat = leftc
-                .iter()
-                .chain(vec![false; fill].iter())
-                .map(|b| *b)
-                .collect::<Vec<bool>>();
-            if let Some(pot) = rules.get(&pat) {
-                new.push_back(*pot);
-            } else {
-                new.push_back(cur[last_pot_checked]);
-            }
-        } else {
-            let pat = leftc
-                .iter()
-                .chain(cur.iter().skip(last_pot_checked + 1).take(2))
-                .map(|b| *b)
-                .collect::<Vec<bool>>();
-            if let Some(pot) = rules.get(&pat) {
-                new.push_back(*pot);
-            } else {
-                new.push_back(cur[last_pot_checked])
-            }
-        }
-        last_pot_checked += 1;
+fn ending(cur: &str, new: &mut String, rules: &Rules, idx: usize) {
+    if idx != cur.len() - 2 {
+        return;
     }
-    let left = cur
-        .iter()
-        .skip(last_pot_checked - 1)
-        .take(2)
-        .map(|b| *b)
-        .collect::<Vec<bool>>();
-    let cright = vec![false; 3];
 
-    let pat = left
-        .iter()
-        .chain(cright.iter())
-        .map(|b| *b)
-        .collect::<Vec<bool>>();
+    for i in 0..4 {
+        let r = match i {
+            0 => [&cur[(idx - 2)..(idx + 2)], "."].concat().to_owned(),
+            1 => [&cur[(idx - 1)..(idx + 2)], ".."].concat().to_owned(),
+            2 => [&cur[idx..(idx + 2)], "..."].concat().to_owned(),
+            3 => [&cur[(idx + 1)..], "...."].concat().to_owned(),
+            _ => return,
+        };
 
-    if let Some(pot) = rules.get(&pat) {
-        if *pot {
-            new.push_back(true);
+        if let Some(&pot) = rules.get(&r) {
+            new.push(pot);
+            println!("{}: {}, {}\n{}", i, &r, pot, cur);
         }
     }
 }
@@ -112,65 +83,48 @@ fn main() {
 
     let input = include_str!("../input");
 
-    let mut cur: Pots = INIT.captures(input.lines().next().unwrap()).unwrap()[1]
-        .chars()
-        .map(|c| c == '#')
-        .collect();
+    let mut cur: String = INIT.captures(input.lines().next().unwrap()).unwrap()[1].to_owned();
 
     for line in input.lines() {
         if let Some(l) = RULE.captures(line) {
-            let br: Vec<bool> = l[1].chars().map(|p| p == '#').collect();
+            let br = l[1].to_owned();
             match &l[2] {
-                "#" => rules.insert(br, true),
-                _ => rules.insert(br, false),
+                "#" => rules.insert(br, '#'),
+                _ => rules.insert(br, '.'),
             };
         }
     }
 
-    let mut tot_negs: isize = 0;
+    let mut leftmost: isize = 0;
     for _ in 0..20 {
-        let mut negs: isize = 0;
-        let mut end: usize = 0;
-        let mut new: Pots = Pots::with_capacity(cur.len() + 5);
-        for (i, w) in cur
-            .iter()
-            .map(|b| *b)
-            .collect::<Vec<bool>>()
-            .windows(5)
-            .enumerate()
-        {
-            end = i;
+        let clen = cur.len();
+        let mut new = String::with_capacity(clen + 5);
+        for (i, _p) in cur.chars().enumerate() {
             if i < 2 {
-                negs += beginning(&cur, i, &rules, &mut new);
+                beginning(&cur, &mut new, &rules, &mut leftmost, i);
+                continue;
             }
 
-            // we're in the middle now
-            if let Some(pot) = rules.get(w) {
-                new.push_back(*pot);
+            if i < clen - 2 {
+                // we're in the middle now
+                let r = &cur[(i - 2)..(i + 3)];
+
+                if let Some(&pot) = rules.get(r) {
+                    new.push(pot);
+                }
             } else {
-                new.push_back(cur[i + 2]);
+                ending(&cur, &mut new, &rules, i);
             }
         }
-        tot_negs += negs;
 
-        ending(&cur, end, &rules, &mut new);
-
-        println!(
-            "{}{}",
-            vec![" "; (40 - negs) as usize].join(""),
-            cur.iter()
-                .map(|c| if *c { "#" } else { "." })
-                .collect::<Vec<_>>()
-                .join("")
-        );
-
-        cur = new;
+        cur = new.trim_matches('.').to_owned();
+        //println!("{}{}", vec![" "; (12 + leftmost) as usize].concat(), cur);
     }
 
     let part_1_answer: isize = cur
-        .iter()
+        .chars()
         .enumerate()
-        .map(|(i, b)| if *b { i as isize - tot_negs } else { 0 })
+        .map(|(i, b)| if b == '#' { i as isize + leftmost } else { 0 })
         .sum();
 
     println!("Part 1: {}", part_1_answer);
